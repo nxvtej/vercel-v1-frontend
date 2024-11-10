46353d2f0207384265998196c2ffd49e78507912
nxvtej <nxvtej@outlook.com>
2024-11-11 00:01:44 +0530

    +

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
+/** @format */
+
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
+	base: "/vercel-v1-frontend",
+
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/vercel-v1-frontend",
+
   plugins: [react()],
   resolve: {
     alias: {
